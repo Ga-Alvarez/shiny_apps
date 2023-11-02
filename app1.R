affa8725fc2740bed667b919863462b9b32c4285
Gabriela Alvarez <alvarezga257@gmail.com>
2023-11-01 19:53:03 -0600

    Se acomodó el código

--- app1.R
+++ app1.R
@@ -2,8 +2,8 @@ library(shiny)
 library(shinydashboard)
 library(readr)
 library(dplyr)
-library(ggplot2)  # Agregamos ggplot2
-library(readxl)
+library(ggplot2)  
+
 
 ui <- navbarPage(
   title = "Análisis de datos des(empleo)",
@@ -14,24 +14,27 @@ ui <- navbarPage(
   ),
   tabPanel(
     title = "Tabla",
-    "Modelos de aprendizaje automático",
-    tableOutput("tabla_empleo")  # Debes renderizar la tabla aquí
+    "Modelos de aprendizaje automático", #cambiar nombre
+    tableOutput("tabla_empleo")  
   ),
   navbarMenu(
     title = "Gráficos",
     tabPanel(
       title = "Análisis por región de empleos agricultura",
-      plotOutput("grafico_barras")  # Cambiamos la renderización de los gráficos
+      plotOutput("grafico_barras")  
     ),
     tabPanel(
       title = "Análisis correlación",
-      selectInput(inputId = "anyo", label = "Escoja un año", choices = unique(datos_empleo$anyo)),
-      actionButton(inputId = "filtrar", label = "Filtrar"),
-      plotOutput("grafico_dispersion")  # Cambiamos la renderización de los gráficos
+      selectInput(inputId = "anyo", 
+                  label = "Escoja un año", 
+                  choices = unique(datos_empleo$anyo)),
+      actionButton(inputId = "filtrar", 
+                   label = "Filtrar"),
+      plotOutput("grafico_dispersion")  
     ),
     tabPanel(
       title = "Analítica Avanzada",
-      plotOutput("grafico")  # Aquí deberías definir la renderización del tercer gráfico
+      plotOutput("grafico")  
     )
   )
 )
@@ -45,25 +48,32 @@ server <- function(input, output, session) {
   
   observe({
     output$grafico_barras <- renderPlot({
-      resumen_datos <- datos_empleo %>%
-        group_by(pais_region) %>%
+      resumen_datos <- datos_empleo  |> 
+        group_by(pais_region) |> 
         summarise(total_agricultura = sum(empleo_agricultura_mujeres + empleo_agricultura_hombres, na.rm = TRUE))
       
       ggplot(data = resumen_datos) +
-        geom_bar(mapping = aes(x = pais_region, y = total_agricultura), stat = "identity", fill = "lightgreen") +
+        geom_bar(mapping = aes(x = pais_region, 
+                               y = total_agricultura), 
+                 stat = "identity", 
+                 fill = "lightgreen") +
         labs(title = "Cantidad de personas que trabajan en la agricultura", x = "Pais/región", y = "Cantidad de personas") +
         theme_minimal() +
         scale_y_continuous(breaks = seq(0, 400000, by = 50000))
     })
     
     observeEvent(input$filtrar, {
-      datos_filtrados <- datos_empleo %>%
+      datos_filtrados <- datos_empleo |> 
         filter(anyo == input$anyo)
       
       if (nrow(datos_filtrados) > 0) {
         grafico_disp <- ggplot(data = datos_filtrados) +
-          geom_point(mapping = aes(x = desempleo_mujeres, y = desempleo_hombres), color = "blue") +
-          labs(title = "Relación entre mujeres y hombres sin empleo", x = "Mujeres desempleadas", y = "Hombres desempleados") +
+          geom_point(mapping = aes(x = desempleo_mujeres,
+                                   y = desempleo_hombres), 
+                     color = "blue") +
+          labs(title = "Relación entre mujeres y hombres sin empleo", 
+               x = "Mujeres desempleadas", 
+               y = "Hombres desempleados") +
           theme_minimal()
         
         output$grafico_dispersion <- renderPlot({
